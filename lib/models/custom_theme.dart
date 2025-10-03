a70adc6829fb5e960d7bc45af0daf33e2c51109d
Lephm <quanleducpham@gmail.com>
2025-10-03 18:40:23 +1000

    remove unecessary files and code

--- lib/models/custom_theme.dart
+++ lib/models/custom_theme.dart
@@ -2,9 +2,8 @@ import 'package:centranews/models/custom_color_scheme.dart';
 import 'package:centranews/models/custom_text_theme.dart';
 
 class CustomTheme {
-  CustomTheme({required currentColorScheme})
-    : currentColorScheme = currentColorScheme,
-      textTheme = CustomTextTheme(currentColorScheme: currentColorScheme);
+  CustomTheme({required this.currentColorScheme})
+    : textTheme = CustomTextTheme(currentColorScheme: currentColorScheme);
 
   CustomColorScheme currentColorScheme;
   CustomTextTheme textTheme;
