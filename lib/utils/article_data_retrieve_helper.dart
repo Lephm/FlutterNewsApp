de018f659598505b9832ade4bd87812dff15e261
Lephm <quanleducpham@gmail.com>
2025-11-12 16:20:43 +1000

    disabling unnecessary check to optimize

--- lib/utils/article_data_retrieve_helper.dart
+++ lib/utils/article_data_retrieve_helper.dart
@@ -1,10 +1,12 @@
 import 'package:centranews/models/article_data.dart';
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 
 // TODO: this is just to make there are no duplicate article data, currently disabling this to improve performance
-List<ArticleData> getUniqueArticleDatas(List<ArticleData> oldList,
-    List<ArticleData> newDataList,) {
-  List<ArticleData> newArticleDatas = [];
+List<ArticleData> getUniqueArticleDatas(
+  List<ArticleData> oldList,
+  List<ArticleData> newDataList,
+) {
+  /*List<ArticleData> newArticleDatas = [];
   newArticleDatas = [...oldList];
   var oldArticleDataIds = oldList.map((articleData) => articleData.articleID);
   for (var data in newDataList) {
@@ -13,6 +15,7 @@ List<ArticleData> getUniqueArticleDatas(List<ArticleData> oldList,
     } else {
       debugPrint("Has removed one article to prevent duplication");
     }
-  }
+  }*/
+  List<ArticleData> newArticleDatas = [...oldList, ...newDataList];
   return newArticleDatas;
 }
